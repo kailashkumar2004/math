a019a715406f58b075833de10b3aabbba0945932
kailash <kailashkumartkg@gmail.com>
2023-07-27 22:07:34 +0530

    initial commit

---
+++ mix_math_qu.js
@@ -0,0 +1,452 @@
+// max==================================
+// var array=[78,90,75,43,21,11,19,23,80,76];
+// console.log(Math.max(...array))
+
+// min==============================================
+// var number=[65,33,23,78,97,1,21,10,39,67,60];
+// console.log(Math.min(...number))
+
+// floor===================================================
+// var a=9.0
+// console.log(Math.floor(a))
+
+// pow======================================
+// var a=9
+// var b=3
+// console.log(Math.pow(a,b))
+
+// round======================================
+// var a=90
+// console.log(Math.round(a))
+
+// spread+oprater==========================================
+// var array=[90,78,65,43,23,12,21,10];
+// function number(a,c,h,l,g){
+//     console.log(a,c,h,l,g)
+
+// }
+// number(...array)
+
+// sqrt===============================================
+// var a=49
+// console.log(Math.sqrt(a))
+
+// rendom_number======================================
+// var a=9
+// var b=100
+// console.log(Math.random(a,b))
+
+// var number=Math.floor(Math.random(Math.random())*100).toString()
+// console.log(number)
+
+// var num=Math.floor(200+Math.random()*500).toString();
+// console.log(num)
+
+// map_loop=================================================
+// var array=[2,5,7,8,9,5,3,2]
+// var newarray=array.map(i=>i*2+i)
+// console.log(newarray)
+
+// forEach_loop==================================================
+
+// var array=[1,2,3,4,5,6,78,90]
+// var max=0
+// array.forEach(function(index,value){
+//     console.log(index,value)
+//     value=value+1
+// })
+
+
+// readuce===================
+// var number=[90,87,65,34,21,14,678,97];
+// var sum=number.reduce(function(acc,curent){
+//     return acc+curent
+// })
+// console.log(sum)
+
+// var array=[12,89,90,100,54,76,25,78,53,42,82];
+// console.log(Math.max(...array))
+
+// var array=[1,90,64,23,84,29,48,59,70];
+// console.log(Math.min(...array))
+
+// var a=9.0
+// console.log(Math.floor(a))
+
+// var a=9
+// var n=3
+// console.log(Math.pow(a,n))
+
+// var num=-9.0
+// console.log(Math.floor(num))
+
+// var a=81
+// console.log(Math.sqrt(a))
+
+// var a=9
+// var b=90
+// console.log(Math.random(a,b))
+
+// var number= Math.floor(Math.random(Math.random())*100).toString();
+// console.log(number)
+
+// var ops=Math.floor(200+Math.random()*900).toString();
+// console.log(ops)
+
+
+
+
+
+
+
+// var array=[12,46,78,90,43,23,100,19,25,67,52];
+// console.log(Math.max(...array))
+
+// var number=[1,90,65,32,56,78,98,76,54,32,12];
+// console.log(Math.min(...number))
+
+// var a=9.0
+// console.log(Math.floor(a))
+
+// var a=9
+// var b=3
+// console.log(Math.pow(a,b))
+
+// var a=625
+// console.log(Math.sqrt(a))
+
+// var a=9
+// var b=20
+// console.log(Math.random(a,b))
+
+
+// var npm=Math.floor(Math.random(Math.random())*200).toString();
+// console.log(npm)
+
+// var ltd=Math.floor(500+Math.random()*1000).toString();
+// console.log(ltd)
+
+// var array=[1,3,4,5,6,7,8,9,11,12,13,19]
+// function mynumber(a,b,c,f,h,k){
+//     console.log(a,b,c,f,h,k)
+// }
+// mynumber(...array)
+
+// var array=[10,20,30,40,50,60,70,80,90];
+// var sum =array.reduce((acc,curent)=>{
+//     return acc+curent
+// });
+// console.log(sum)
+
+
+
+
+
+
+
+
+// var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 34, 56, 78, 90, 41];
+// var sum = array.reduce((acc, current) => {
+//         return acc + current
+// })
+// console.log(sum)
+
+// var array=[10,20,30,40,50,60,70,80,90]
+// function mynumber(p,q,r,s){
+//     console.log(p,q,r,s)
+// }
+// mynumber(...array)
+
+// var array=[10,100,28,94,50,60,71];
+// console.log(Math.max(...array))
+
+// var array=[90,41,24,67,54,32,1,67,86];
+// console.log(Math.min(...array))
+
+// var a=1
+// var b=100
+// console.log(Math.random(a,b))
+
+// var csp =Math.floor(Math.random(Math.random())*190).toString();
+// console.log(csp)
+
+// var number=Math.floor(300+Math.random()*900).toString();
+// console.log(number)
+
+// var a=7.6
+// console.log(Math.floor(a))
+
+// var a=5
+// var b=4
+// console.log(Math.pow(a,b))
+
+// var n=729
+// console.log(Math.sqrt(n))
+
+
+// var array = [1, 54, 86, 32, 24, 67, 90, 100, 23]
+// console.log(Math.max(...array))
+
+// var array = [1, 98, 54, 23, 45, 67, 80]
+// console.log(Math.min(...array))
+
+// a = 9.0
+// console.log(Math.floor(a))
+
+// var a = 9
+// var b = 3
+// console.log(Math.pow(a,b))
+
+// a = 9.8
+// console.log(Math.trunc(a))
+
+// var a = 729
+// console.log(Math.sqrt(a))
+
+// a = 9
+// b = 89
+// console.log(Math.random(a,b))
+
+// var mtp = Math.floor(Math.random(Math.random()) * 100).toString()
+// console.log(mtp)
+
+// var num=Math.floor(500+Math.random()*600).toString()
+// console.log(num)
+
+// var array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// function mynumber(a, b, c, d) {
+//     console.log(a,b,c,d)
+// }
+// mynumber(...array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var array = [1, 2, 3, 4, 5, 6, 7, 99, 100, 58, 97, 43, 25, 46];
+// console.log(Math.max(...array))
+
+// var numbe = [98, 45, 63, 23, 56, 89, 2, 1, 22, 11, 20, 76, 43, 47]
+// console.log(Math.min(...numbe))
+
+// var num = 9.9
+// console.log(Math.floor(num))
+
+// var a = 9
+// var b=4
+// console.log(Math.pow(a,b))
+
+// var a = 96
+// console.log(Math.trunc(a))
+
+// var num = 625
+// console.log(Math.sqrt(num))
+
+// var a = 6
+// var n = 80
+// console.log(Math.random(a,n))
+
+// var api = Math.floor(Math.random(Math.random) * 250).toString();
+// console.log(api)
+
+// var number = Math.floor(200 + Math.random() * 700).toString();
+// console.log(number)
+
+
+// var array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// var sum = array.reduce((acc, current) => {
+//     return acc+current
+// })
+// console.log(sum)
+
+// var num = [10, 26, 79, 45, 73, 49, 87, 61];
+// function mynumber(p, q, r, s) {
+//     console.log(p,q,r,s)
+// }
+// mynumber(...num)
+
+// setTimeout(function () {
+//     var array = [1, 3, 5, 6, 7, 8, 9, 21, 34, 56, 78, 90, 83, 2, 3, 5, 6, 7, 9, 1];
+//     var uniqueNumber = []
+//     for (var i = 0; i < array.length; i++){
+//         if (uniqueNumber.indexOf(array[i]) == -1) {
+//             uniqueNumber.push(array[i])
+//         }
+//     }
+//     console.log(uniqueNumber)
+// }, 9000)
+// function lastNumber() {
+//     var array = [12, 28, 20, 20, 15, 25, 34, 56, 78, 93, 21, 70, 64, 35, 46, 50];
+//     var num = 40
+//     var emptyarray = [];
+//     for (var i of array) {
+//         for (var j of array) {
+//             if (i + j == num) {
+//                 emptyarray.push([i,j])
+//             }
+//         }
+//     }
+//     console.log(emptyarray)
+// }
+// setTimeout(lastNumber,9000)
+
+
+// var number=new Promise((resolve, reject) => {
+//     var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 21, 23, 32, 34, 43, 45, 54];
+//     var evennumber = []
+//     var i = 0
+//     while (i < array.length) {
+//         if (array[i] % 2 == 0) {
+//             evennumber.push(array[i])
+//         }
+//         i=i+1
+//     }
+//     if (evennumber.length > 0) {
+//         resolve(evennumber)
+//     }
+//     else {
+//         reject("not even")
+//     }
+// })
+// number.then((even) => {
+//     console.log("even================",even)
+// }).catch((error) => {
+//     console.log("error=================",error)
+// }).finally((heloo) => {
+//     console.log("heloo==================",heloo)
+// })
+
+
+// var promise1 = new Promise((resolve, reject) => {
+//     var number = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+//     var sum = 0
+//     for (var i in number) {
+//         sum = sum + number[i]
+//     }
+//     if (sum !== undefined) {
+//         setTimeout(() => {
+//             resolve(sum)
+//         }, 7000)
+//     }
+//     else {
+//         reject("kailash")
+//     }
+// })
+// var promise2 = new Promise((resolve, reject) => {
+//     var array = [11, 12, 34, 56, 78, 90, 16, 21, 23, 43, 54];
+//     var max = array[0]
+//     for (var i in array) {
+//         if (array[i] > max) {
+//             max = array[i]
+//         }
+//         if (max !== undefined) {
+//             setTimeout(() => {
+//                 resolve(max)
+//             }, 4000)
+//         }
+//         else {
+//             reject("num not max")
+//         }
+//     }
+
+
+// })
+// var all_promise = Promise.all([promise1, promise2])
+// all_promise.then((result) => {
+//     console.log(result)
+// })
+
+// var new_number=new Promise((resolve, reject) => {
+//     var array = [23, 5, 6, 78, 43, 90, 71, 32, 43, 54, 1];
+//     var min = array[0]
+//     for (var i in array) {
+//         if (array[i] < min) {
+//             min=array[i]
+//         }
+//         if (min !== undefined) {
+//             setTimeout(() => {
+//                 resolve(min)
+//             },5000)
+//         }
+//         else {
+//             reject("not miv")
+//         }
+//     }
+// })
+// new_number.then((min) => {
+//     console.log("min==================", min)
+//     return new Promise((resolve, reject) => {
+//         var array1 = [19, 30, 40, 57, 2, 43, 56, 78, 90, 11, 21, 32, 43, 76];
+//         var array2 = [76, 43, 98, 11, 12, 2, 34, 45, 67, 89, 65, 110, 17, 30];
+//         var emptyarray = []
+//         for (var i of array1) {
+//             if (array2.includes(i)) {
+//                     emptyarray.push(i)
+//                 }
+//         }
+//         if (emptyarray !== undefined) {
+//             resolve(emptyarray)
+//         }
+//         else {
+//             reject("hello word")
+//         }
+        
+//     }).then((emptyarray) => {
+//         console.log("emptyarray==============", emptyarray)
+//     }).catch((error) => {
+//         console.log("error==============",error)
+//     })
+// })
+
+
+// var a = 9.0
+// console.log(Math.floor(a))
+
+// var num1 = 4.9
+// console.log(Math.abs(num1))
+
+// var array = [1, 78, 90, 65, 43, 21, 100, 78, 45];
+// console.log(Math.max(...array))
+
+// var number = [97, 54, 32, 1, 21, 12, 34, 76, 50];
+// console.log(Math.min(...number))
+
+// var a = 9
+// var b = 3
+// console.log(Math.pow(a,b))
+
+// var num = 49
+// console.log(Math.sqrt(num))
+
+// var num1 = 1
+// var num2 = 80
+// console.log(Math.random(num1,num2))
+
+// var number = Math.floor(Math.random(Math.random) * 200).toString();
+// console.log(number)
+
+// var otp = Math.floor(Math.random(Math.random) * 100).toString();
+// console.log(otp)
+
+// var lpg = Math.floor(200 + Math.random() * 700).toString();
+// console.log(lpg)
+
+// var array = [12, 34, 56, 78, 90, 86, 54, 32, 10, 20, 47];
+// var sum = array.reduce((acc, current) => {
+//     return acc+current
+// })
+// console.log(sum)
+
+// var array = [9, 8, 7, 6, 12, 5, 4, 3, 2, 1];
+// function mynumber(a, b, c, f, y) {
+//     console.log(a,b,c,f,y)
+// }
+// mynumber(...array)
